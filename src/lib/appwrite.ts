9172efe04e1191332bb4a02f0f9ba66ef883fbb7
RangerDevv <meerabdul2008@gmail.com>
2023-05-18 09:01:53 -0400

    Integration w/ Astro is fully functional

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,1 +1,23 @@
+import { Client, Account, ID, Databases, Permission, Role, Teams, Storage, Models } from 'appwrite';
 
+export const appwriteClient = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('646538bf8be3792191bd'); // Your project ID
+
+export const appwriteDatabases = new Databases(appwriteClient);
+export const appwriteStorage = new Storage(appwriteClient);
+
+export interface Products extends Models.Document {
+    Name: string;
+    Author: string;
+    LaunchDate: string;
+    Tagline: string;
+    Description: string;
+    Free: boolean;
+    Icon: string;
+    Thumbnail: string;
+    Link?: string;
+}
+
+
+export interface ProductList extends Models.DocumentList<Products> {}
