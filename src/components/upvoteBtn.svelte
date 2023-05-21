a5fb48eefcd0fde2a512903f8e20a6e78990ecf7
RangerDevv <meerabdul2008@gmail.com>
2023-05-21 16:16:23 -0400

    trying upvote functionality

---
+++ src/components/upvoteBtn.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import {appwriteUser,appwriteDatabases} from '../lib/appwrite';
+  
+import { ID } from 'appwrite';
+
+export let DatabaseID = '';
+export let CollectionID = '';
+export let DocumentID = '';
+
+function upvote(docID: any) {
+
+    const promise = appwriteDatabases.updateDocument(DatabaseID,CollectionID,DocumentID,{
+        'Upvotes': docID.Upvotes + 1
+    });
+
+    promise.then((response) => {
+
+        console.log(response);
+
+    }, (error) => {
+
+        console.log(error);
+
+        alert(error.message);
+
+    });
+
+}
+
+</script>
+
+<button on:click={() => upvote(DocumentID)} class="bg-purple-600 hover:bg-purple-700 text-white">Upvote</button>
