ee0a9651c42636fe506d4ca6c1c4ca0a00e6c108
RangerDevv <meerabdul2008@gmail.com>
2023-05-21 15:33:52 -0400

    Remove the userName cookie when logged out

--- src/components/signout.svelte
+++ src/components/signout.svelte
@@ -11,6 +11,7 @@ promise.then((response) => {
     document.cookie = `user=; expires=${new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`;
     document.cookie = `userName=; expires=${new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`;
     document.cookie = `session=; expires=${new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`;
+    document.cookie = `userName=; expires=${new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`;
     window.location.href = '/';
 }, (error) => {
     console.log(error);
