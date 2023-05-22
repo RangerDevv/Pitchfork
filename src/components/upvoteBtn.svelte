34c95383b6dd75878cb88017009028c24a997bce
RangerDevv <meerabdul2008@gmail.com>
2023-05-21 21:07:29 -0400

    user voting updating

--- src/components/upvoteBtn.svelte
+++ src/components/upvoteBtn.svelte
@@ -40,8 +40,9 @@ async function upvote(docID:any){
     });
       console.log(docID);
       voted = true;
-
-  
+      UpvoteCount = doc.Upvotes.length;
+      voted = doc.Upvotes.includes(uid);
+      
   } catch (error : any) {
       console.log(error);
       alert(error.message);
