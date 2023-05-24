07ac5a58610172cb2ca6dcc615190c2c8fba4ea2
RangerDevv <meerabdul2008@gmail.com>
2023-05-24 09:02:44 -0400

    fixed a bug with icon upload for pitch

--- src/components/create.svelte
+++ src/components/create.svelte
@@ -332,7 +332,7 @@ async function createPitch() {
           file:rounded-md file:border-0
           file:text-sm file:font-semibold
           file:bg-blue-500 file:text-white
-          hover:file:bg-blue-600" type="file" id="IconFile" />
+          hover:file:bg-blue-600" type="file" id="PitchIconFile" />
         <label for="ThumbnailFile" class="block text-sm font-medium text-gray-100">Thumbnail</label>
         <input class="block text-sm text-gray-300
           file:mr-4 file:py-2 file:px-4
