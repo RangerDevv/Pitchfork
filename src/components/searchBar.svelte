8dfdf13755c25250b241f646710cf596662c8d26
RangerDevv <meerabdul2008@gmail.com>
2023-05-29 18:25:39 -0400

    Refining the search UI

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -20,26 +20,39 @@ async function search() {
 
 </script>
 
+<!-- <div>
 <div class="form-control">
     <label class="input-group">
       <span>Search</span>
       <input type="text" placeholder="ReWrit" class="input input-bordered w-96" bind:value={searchTerms} on:input={search} />
     </label>
 </div>
-<div class="flex flex-col space-y-2 justify-center items-center">
-    {#each PitchResults.documents as PitchResult}
-        <div class="flex flex-row gap-6">
-			<!-- svelte-ignore a11y-img-redundant-alt -->
-			<img src={'https://cloud.appwrite.io/v1/storage/buckets/646547f5019189c8092b/files/'+PitchResult.Icon+'/preview?project=646538bf8be3792191bd'} alt="image" class="w-auto h-24" />
-            <div class="flex flex-col space-y-2">
-                <p class="text-2xl font-bold">{PitchResult.Title}</p>
-                <p class="text-xl">{PitchResult.Tagline}</p>
-            </div>
-            <!--  upvotes count -->
-            <div class="flex flex-col space-y-2">
-                <p class="text-2xl font-bold">{PitchResult.Upvotes.length}</p>
-                <p class="text-xl">Upvotes</p>
+</div> -->
+<!-- The button to open modal -->
+<label for="my-modal-3" class="btn">Search</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box relative">
+    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <div class="form-control">
+        <label class="input-group">
+          <span>Search</span>
+          <input type="text" placeholder="ReWrit" class="input input-bordered w-96" bind:value={searchTerms} on:input={search} />
+        </label>
+    </div>
+    {#if PitchResults}
+    {#each PitchResults.documents as Pitch}
+        <div class="flex flex-col">
+            <div class="flex flex-row">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <img src={'https://cloud.appwrite.io/v1/storage/buckets/646547f5019189c8092b/files/'+Pitch.Icon+'/preview?project=646538bf8be3792191bd'} alt="image" class="w-auto h-24" />
+                <h1 class="text-2xl font-bold">{Pitch.Title}</h1>
+                <h1 class="text-2xl font-bold">{Pitch.Tagline}</h1>
             </div>
         </div>
     {/each}
+    {/if}
+  </div>
 </div>
