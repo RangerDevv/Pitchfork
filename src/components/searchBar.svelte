e071ed34c5f11ec6a26aacb7e89aa1b8192884df
RangerDevv <meerabdul2008@gmail.com>
2023-05-29 18:14:01 -0400

    fixed the title bug and testing search results

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -4,17 +4,18 @@ import { appwriteClient,appwriteDatabases,appwriteStorage} from "../lib/appwrite
 import { Query } from "appwrite";
 
 let searchTerms = "";
-let searchResults = [] as any;
+let PitchResults = [] as any;
+let postResults = [] as any;
 
 async function search() {
-    searchResults = await appwriteDatabases.listDocuments(
+    PitchResults = await appwriteDatabases.listDocuments(
     '646b9dd716753e384863',
 	'646b9e0e29b66e4a8a22',
 	[
         Query.search("Title", searchTerms)
 	]
     );
-    console.log(searchResults);
+    console.log(PitchResults);
 }
 
 </script>
@@ -24,4 +25,21 @@ async function search() {
       <span>Search</span>
       <input type="text" placeholder="ReWrit" class="input input-bordered w-96" bind:value={searchTerms} on:input={search} />
     </label>
+</div>
+<div class="flex flex-col space-y-2 justify-center items-center">
+    {#each PitchResults.documents as PitchResult}
+        <div class="flex flex-row gap-6">
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img src={'https://cloud.appwrite.io/v1/storage/buckets/646547f5019189c8092b/files/'+PitchResult.Icon+'/preview?project=646538bf8be3792191bd'} alt="image" class="w-auto h-24" />
+            <div class="flex flex-col space-y-2">
+                <p class="text-2xl font-bold">{PitchResult.Title}</p>
+                <p class="text-xl">{PitchResult.Tagline}</p>
+            </div>
+            <!--  upvotes count -->
+            <div class="flex flex-col space-y-2">
+                <p class="text-2xl font-bold">{PitchResult.Upvotes.length}</p>
+                <p class="text-xl">Upvotes</p>
+            </div>
+        </div>
+    {/each}
 </div>
