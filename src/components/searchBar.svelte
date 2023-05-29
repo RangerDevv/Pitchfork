ac99531f06e8777d5586e9bc0dca5765b0382d17
RangerDevv <meerabdul2008@gmail.com>
2023-05-29 19:00:56 -0400

    Search feature should be fully functional

--- src/components/searchBar.svelte
+++ src/components/searchBar.svelte
@@ -55,18 +55,32 @@ async function search() {
           <input type="text" placeholder="ReWrit" class="input input-bordered w-96" bind:value={searchTerms} on:input={search} />
         </label>
     </div>
+        <!--  mock array of results to test the UI -->
+        <!-- <div class="flex flex-col">
+            <div class="flex flex-row justify-between items-center space-x-4"> -->
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <!-- <img src="https://picsum.photos/200/200" alt="image" class="w-auto h-24" />
+                <div class="flex flex-col w-auto">
+                    <h1 class="text-lg font-semibold">ReWrit</h1>
+                    <h1 class="text-md font-medium text-gray-400">ReWrit is a platform for writers to share their work and get feedback from other writers.</h1>
+                </div>
+            </div>
+        </div> -->
+    <div class="overflow-y-auto h-96"> <!-- This is the div that will scroll -->
     {#if PitchResults.documents != undefined && PitchResults.documents.length > 0}
-    <p class="text-2xl font-bold">Results</p>
     {#each PitchResults.documents as Pitch}
         <div class="flex flex-col">
-            <div class="flex flex-row">
+            <div class="flex flex-row justify-between items-center space-x-4">
                 <!-- svelte-ignore a11y-img-redundant-alt -->
-                <img src={'https://cloud.appwrite.io/v1/storage/buckets/646547f5019189c8092b/files/'+Pitch.Icon+'/preview?project=646538bf8be3792191bd'} alt="image" class="w-auto h-24" />
-                <h1 class="text-2xl font-bold">{Pitch.Title}</h1>
-                <h1 class="text-2xl font-bold">{Pitch.Tagline}</h1>
+                <img src={'https://cloud.appwrite.io/v1/storage/buckets/646547f5019189c8092b/files/'+Pitch.Icon+'/preview?project=646538bf8be3792191bd'} alt="image" class="w-auto h-16" />
+                <div class="flex flex-col w-auto">
+                    <h1 class="text-lg font-semibold">{Pitch.Title}</h1>
+                    <h1 class="text-md font-medium text-gray-400">{Pitch.Tagline}</h1>
+                </div>
             </div>
         </div>
     {/each}
     {/if}
+    </div>
   </div>
 </div>
