579da4e7df97f30a6710733e47c9afaa3e3aa82b
RangerDevv <meerabdul2008@gmail.com>
2023-05-29 18:01:16 -0400

    Implementing and testing search feature

---
+++ src/components/searchBar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { appwriteClient,appwriteDatabases,appwriteStorage} from "../lib/appwrite";
+
+import { Query } from "appwrite";
+
+let searchTerms = "";
+let searchResults = [] as any;
+
+async function search() {
+    searchResults = await appwriteDatabases.listDocuments(
+    '646b9dd716753e384863',
+	'646b9e0e29b66e4a8a22',
+	[
+        Query.search("Search", searchTerms)
+	]
+    );
+    console.log(searchResults);
+}
+
+</script>
+
+<div class="form-control">
+    <label class="input-group">
+      <span>Search</span>
+      <input type="text" placeholder="ReWrit" class="input input-bordered w-96" bind:value={searchTerms} on:input={search} />
+    </label>
+</div>
