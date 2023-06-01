c638e504fa9fed1ddb2c166e158276ebb240597b
RangerDevv <meerabdul2008@gmail.com>
2023-06-01 09:03:28 -0400

    Updating the comments section UI

--- src/components/comments.svelte
+++ src/components/comments.svelte
@@ -42,30 +42,38 @@
             AuthorName: userDisplayName,
             },
         );
+        await promise;
     }
 </script>
 
 <main>
     <p class="text-2xl font-bold text-center pt-10 pb-10">Comments</p>
-    <div class="flex flex-row space-x-4">
+    <div class="flex flex-row space-x-4 items-center mx-auto w-96">
         <textarea
             class="border-2 border-transparent rounded-lg p-2 w-96"
             rows="1"
             bind:value={postedComment}
         ></textarea>
         <button
-            class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-2 rounded"
             on:click={postComment}>
-            Post Comment
+            📄
         </button>
     </div>
-    <div class="flex flex-col space-y-4">
+    <div class="flex flex-col space-y-4 p-8">
     {#if comments.length === 0}
         <p class="text-center">No comments yet</p>
     {:else}
     {#each comments as comment}
         <div class="flex flex-col space-y-2">
-        <p class="text-lg font-bold">{comment.AuthorName}</p>
+        <div class="flex flex-row space-x-2">
+            <img
+            class="rounded-full h-8 w-8"
+            src="https://avatars.dicebear.com/api/avataaars/${comment.AuthorUid}.svg"
+            alt="avatar"
+            />
+            <p class="text-lg font-bold">{comment.AuthorName}</p>
+        </div>
         <p class="text-sm">{comment.Comment}</p>
         </div>
     {/each}
