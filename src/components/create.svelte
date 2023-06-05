f74b2b67fdaa0447affa148b13c57c875c066cf1
RangerDevv <meerabdul2008@gmail.com>
2023-06-05 10:27:47 -0400

    Typos and bug fixes

--- src/components/create.svelte
+++ src/components/create.svelte
@@ -133,7 +133,7 @@ async function CreatePost() {
 <main class="flex flex-row justify-evenly p-10">
     <div>
       <div class="flex flex-col space-y-2 items-center pt-20 h-[100vh]" id="Start">
-        <p class="text-gray-100 text-center text-2xl pb-2 w-96">Hello! This is the pitch creation page. Here you will be able to create a pitch for your project. This pitch will be displayed on the home page of the website. Please fill out the form below to create your pitch.</p>
+        <p class="text-gray-100 text-center text-2xl pb-2 w-96">Hello! This is the product creation page. Here you will be able to create a product for your project. This product will be displayed on the home page of the website. Please fill out the form below to create your product.</p>
         <a href="#Title" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
         </div>
         <div class=" mb-5 items-center justify-center flex flex-col space-y-2">
@@ -148,22 +148,26 @@ async function CreatePost() {
             <a href="#Description" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Description">
-            <p class="text-gray-100 text-left text-2xl pb-2">Provide a description of your pitch.</p>
+            <p class="text-gray-100 text-left text-2xl pb-2">Provide a description of your product.</p>
             <textarea rows="6" bind:value={PostDescription} placeholder="Description" class="bg-transparent outline outline-gray-500 w-64 sm:w-96 rounded-md p-2"></textarea>
             <a href="#Link" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Link">
-            <p class="text-gray-100 text-left text-2xl pb-2">Provide a link to your pitch.</p>
+            <p class="text-gray-100 text-left text-2xl pb-2">Provide a link to your product.</p>
             <input type="text" class="bg-transparent input-lg outline outline-purple-500 w-64 sm:w-96 h-10 rounded-md p-2" bind:value={PostLink} placeholder="Link" />
             <a href="#Free" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Free">
-            <p class="text-gray-100 text-left text-2xl pb-2">Is your pitch free?</p>
-            <div class="flex flex-row gap-6">
-            <input type="radio" class="bg-transparent input-lg outline outline-purple-500 w-64 sm:w-96 h-10 rounded-md p-2" bind:value={IsFree} />
+            <p class="text-gray-100 text-left text-2xl pb-2">Is your product free?</p>
+            <div class="form-control">
+                <label class="cursor-pointer label">
+                  <input type="checkbox" class="checkbox checkbox-lg checkbox-success" bind:checked={IsFree}/>
+                </label>
+              </div>
+              <a href="#LaunchDate" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="LaunchDate">
-            <p class="text-gray-100 text-left text-2xl pb-2">When do you plan to launch your pitch?</p>
+            <p class="text-gray-100 text-left text-2xl pb-2">When do you plan to launch your product?</p>
             <div class="flex flex-row gap-6">
             <!-- svelte-ignore a11y-label-has-associated-control -->
             <input class="bg-transparent text-xl outline-none bg-slate-700 rounded-md p-2" type="date" bind:value={PostLaunchDate}/>
@@ -171,21 +175,21 @@ async function CreatePost() {
             <a href="#Icon" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Icon">
-            <p class="text-gray-100 text-left text-2xl pb-2">Upload an icon for your pitch.</p>
+            <p class="text-gray-100 text-left text-2xl pb-2">Upload an icon for your product.</p>
             <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" id="IconFile" accept="png, jpg, jpeg, gif, svg" />
             <a href="#Thumbnail" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
             <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Thumbnail">
-            <p class="text-gray-100 text-left text-2xl pb-2">Upload a thumbnail for your pitch. (Must have at least 1)</p>
+            <p class="text-gray-100 text-left text-2xl pb-2">Upload a thumbnail for your product. (Must have at least 1)</p>
             <label for="ThumbnailFile" class="block text-sm font-medium text-gray-100">Required</label>
             <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" id="ThumbnailFile" accept="png, jpg, jpeg, gif, svg" required />
             <label for="ThumbnailFile2" class="block text-sm font-medium text-gray-100">Optional</label>
             <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" id="ThumbnailFile2" accept="png, jpg, jpeg, gif, svg" />
             <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" id="ThumbnailFile3" accept="png, jpg, jpeg, gif, svg" />
             <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" id="ThumbnailFile4" accept="png, jpg, jpeg, gif, svg" />
-            <a href="#Pitch" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
+            <a href="#product" class="text-gray-100 text-xl mt-7 btn btn-primary normal-case">Next</a>
             </div>
-            <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="Pitch">
+            <div class="flex flex-col space-y-2 justify-center items-center h-[100vh]" id="product">
             <p class="text-gray-100 text-left text-2xl pb-2">Are You Ready?</p>
             <div class="flex flex-row justify-center items-center mt-5">
             <button on:click={CreatePost} class="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md self-center mx-auto my-auto">Launch It 🚀</button>
